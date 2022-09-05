5bf678bca131a4a651979c0de4a09903b8a9eecf
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-09-06 02:25:46 +0430

    Add close functionality to menu items

--- Mobile.js
+++ Mobile.js
@@ -5,3 +5,21 @@ function Deploy_menu(){
     hambuermenu.style.display="block";
 
 }
+
+
+document.getElementById("close-btn").addEventListener("click", Close_menu);
+function Close_menu(){
+    let hambuermenu = document.getElementById("mobile-menu");
+    hambuermenu.style.display="none";
+
+}
+
+
+const elements=document.getElementsByClassName("line_border")
+const elements_array= Array.from(elements)
+elements_array.forEach(function(Element){
+Element.addEventListener("click", Close_menu);
+})
+
+
+
