0f8632bfb0fcee60a5febc1a40caf1c9fd7e1aa7
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-05 16:29:57 -0500

    Add Mobile-Menu-Open

---
+++ Mobile.js
@@ -0,0 +1,7 @@
+
+document.getElementById("Deploy_menu").addEventListener("click", Deploy_menu);
+function Deploy_menu(){
+    let hambuermenu = document.getElementById("mobile-menu");
+    hambuermenu.style.display="block";
+
+}
