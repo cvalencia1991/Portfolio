9610fc6822e3494f55cf7cc19219db1d92a6a981
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-07 10:59:34 -0500

    Edit Card Changes

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-vars */
 /* eslint-disable guard-for-in */
 const projects = [
   {
@@ -63,7 +65,6 @@ const projects = [
 
 ];
 
-// eslint-disable-next-line no-unused-vars
 function deploypopup(x) {
   const buttonCard = document.getElementById('popupmobile');
   const titulo = document.getElementById('titulo');
@@ -97,18 +98,18 @@ function cardworksection() {
   // eslint-disable-next-line no-unused-vars, no-restricted-syntax
   for (const project in projects) {
     const nameproject = projects[project].name;
-    const description = projects[project];
-    const featuredimage = projects[project];
-    const tech1 = projects[project];
-    const tech2 = projects[project];
-    const tech3 = projects[project];
+    const description = 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard';
+    const featuredimage = projects[project].featuredimage;
+    const tech1 = projects[project].tech1;
+    const tech2 = projects[project].tech2;
+    const tech3 = projects[project].tech3;
     const card = `<div class="grid2">
     <img src="${featuredimage}" alt="" class="styleimg">
     <div class="lay-on-top">
         <h3 class="card-header">${nameproject}</h3>
         <p class="description">${description}</p>
         <ul class="technology">
-            <li class=>${tech1}</li>
+            <li class="html1">${tech1}</li>
             <li class="boostrap">${tech2}</li>
             <li class="ruby">${tech3}</li>
         </ul>
