8b36aa281d5f97e73cb886afce931b451fc3e789
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-08 14:48:16 -0500

    Edit Files with linters

--- LocalStorage.js
+++ LocalStorage.js
@@ -1,23 +1,24 @@
-function saveinfomation(){
-     let nameform = document.getElementById('name').value;
-     let email = document.getElementById('email').value;
-     let message = document.getElementById('messageform').value;
+// eslint-disable-next-line no-unused-vars
+function saveinfomation() {
+  const nameform = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('messageform').value;
 
-     const task = {
-          nameform,
-          email,
-          message
-     }
-     localStorage.setItem('information',JSON.stringify(task));
+  const task = {
+    nameform,
+    email,
+    message,
+  };
+  localStorage.setItem('information', JSON.stringify(task));
 }
 
-function loadDataFromLocalStorage(){
-     if(localStorage.getItem('information')!== null){
-          let tasks = JSON.parse(localStorage.getItem('information'));
-          document.getElementById('name').value = tasks.nameform;
-          document.getElementById('email').value = tasks.email;
-          document.getElementById('messageform').value = tasks.message;
-     }
+function loadDataFromLocalStorage() {
+  if (localStorage.getItem('information') !== null) {
+    const tasks = JSON.parse(localStorage.getItem('information'));
+    document.getElementById('name').value = tasks.nameform;
+    document.getElementById('email').value = tasks.email;
+    document.getElementById('messageform').value = tasks.message;
+  }
 }
 
 loadDataFromLocalStorage();
--- validationform.js
+++ validationform.js
@@ -1,90 +1,80 @@
 const form = document.getElementById('contactfinalform');
-
-form.addEventListener('submit', check);
-
-function check(e){
-     e.preventDefault();
-     if(validationform()){
-          // submit the form
-          saveinfomation()
-          //form.submit();
-     }
-
-}
+const emailElement = document.getElementById('email');
 
 function validationform() {
-     let nameform = document.getElementById('name');
-     let email = document.getElementById('email');
-     let message = document.getElementById('messageform');
+  const nameform = document.getElementById('name');
+  const email = document.getElementById('email');
+  const message = document.getElementById('messageform');
 
-     nameform.value = nameform.value.trim();
-     email.value = email.value.trim();
-     message.value = message.value.trim();
+  nameform.value = nameform.value.trim();
+  email.value = email.value.trim();
+  message.value = message.value.trim();
 
-     if(nameform.value === '') {
-          SetError(nameform,'Username is required');
-     } else {
-          SetSuccess (nameform);
-     }
-     if (email.value=== '') {
-          SetError(email,'email is required');
-     } else if(!isValidEmail(email.value)) {
-          SetError(email,'provide a valid email adress');
-     } else {
-          SetSuccess (email);
-     }
+  function isValidEmail(email) {
+    const re = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+    return re.test(String(email).toLowerCase());
+  }
 
-     if (message.value=== '') {
-          SetError(message,'message is required');
-     } else {
-          SetSuccess (message);
-     }
+  function SetError(element, message) {
+    const errorDisplay = element.previousElementSibling;
+    errorDisplay.innerText = message;
+    element.classList.add('error');
+    element.classList.remove('success');
+  }
 
-     let isValid=true;
-     if(nameform.classList.contains('error') || email.classList.contains('error') || message.classList.contains('error')) {
-          isValid = false
-     }
-     return isValid
+  function SetSuccess(element) {
+    const errorDisplay = element.previousElementSibling;
+    errorDisplay.innerText = '';
+    element.classList.add('success');
+    element.classList.remove('error');
+  }
+
+  function checkEmail() {
+    const errorDisplay = emailElement.previousElementSibling;
+    const str = emailElement.value;
+    if (str !== str.toLowerCase()) {
+      errorDisplay.innerText = 'Email has to be in lower case';
+      emailElement.classList.add('error');
+      emailElement.classList.remove('success');
+    } else {
+      errorDisplay.innerText = '';
+      emailElement.classList.remove('error');
+    }
+  }
+  emailElement.addEventListener('input', checkEmail);
+
+  if (nameform.value === '') {
+    SetError(nameform, 'Username is required');
+  } else {
+    SetSuccess(nameform);
+  }
+  if (email.value === '') {
+    SetError(email, 'email is required');
+  } else if (!isValidEmail(email.value)) {
+    SetError(email, 'provide a valid email adress');
+  } else {
+    SetSuccess(email);
+  }
+
+  if (message.value === '') {
+    SetError(message, 'message is required');
+  } else {
+    SetSuccess(message);
+  }
+
+  let isValid = true;
+  if (nameform.classList.contains('error') || email.classList.contains('error') || message.classList.contains('error')) {
+    isValid = false;
+  }
+  return isValid;
 }
 
-
-function isValidEmail (email) {
-     const re =/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
-     //const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
-     return re.test(String(email).toLowerCase());
- }
-
-function SetError (element, message) {
-     const inputControl = element.parentElement;
-     const errorDisplay = element.previousElementSibling;
-
-     errorDisplay.innerText = message;
-     element.classList.add('error');
-     element.classList.remove('success');
+function check(e) {
+  e.preventDefault();
+  validationform();
+  /* if (validationform()) {
+    // eslint-disable-next-line no-undef
+    /* saveinfomation(); */
 }
 
-function SetSuccess (element) {
-     const inputControl = element.parentElement;
-     const errorDisplay = element.previousElementSibling;
-
-     errorDisplay.innerText='';
-     element.classList.add('success');
-     element.classList.remove('error');
-}
-
-function checkEmail(){
-     let str = emailElement.value;
-
-     const errorDisplay = emailElement.previousElementSibling;
-     if(str!==str.toLowerCase()){
-          errorDisplay.innerText='Email has to be in lower case';
-          emailElement.classList.add('error');
-          emailElement.classList.remove('success');
-     } else{
-          errorDisplay.innerText='';
-          emailElement.classList.remove('error');
-     }
-}
-
-const emailElement = document.getElementById('email');
-emailElement.addEventListener('input', checkEmail);
+form.addEventListener('submit', check);
