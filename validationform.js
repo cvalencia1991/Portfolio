81daf6e29623e2478fb0b4c493b28b562c03fc66
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-09-08 21:08:31 +0430

    Fix form validation

--- validationform.js
+++ validationform.js
@@ -1,106 +1,90 @@
 const form = document.getElementById('contactfinalform');
 
-
 form.addEventListener('submit', check);
-
 
 function check(e){
      e.preventDefault();
-   /*   validationform(); */
-     saveinfomation();
-}
-
-function validationform(){
-     let nameform = document.getElementById('name').value;
-     let email = document.getElementById('email').value;
-     let message = document.getElementById('messageform').value;
-
-
-}
-
-function saveinfomation(){
-     let nameform = document.getElementById('name').value;
-     let email = document.getElementById('email').value;
-     let message = document.getElementById('messageform').value;
-
-     const task = {
-          nameform,
-          email,
-          message
+     if(validationform()){
+          // submit the form
+          saveinfomation()
+          //form.submit();
      }
 
-     if(localStorage.getItem('information')!== null){
-          //let tasks = JSON.parse(localStorage.getItem('information'));
-          //tasks.push(task);
-          localStorage.setItem('information',JSON.stringify(task));
-     }else {
-               //let tasks = JSON.parse(localStorage.getItem('information'));
-               //tasks.push(task);
-               localStorage.setItem('information',JSON.stringify(task));
-     }
-
-
-     // if(localStorage.getItem('information')=== null){
-     //      let tasks= [];
-     //      tasks.push(task);
-     //      localStorage.setItem('information',JSON.stringify(tasks));
-     // } else {
-     //      let tasks = JSON.parse(localStorage.getItem('information'));
-     //      tasks.push(task);
-     //      localStorage.setItem('information',JSON.stringify(tasks));
-     // }
-   /*  console.log(JSON.parse(localStorage.getItem('infomation'))) convert the infomation into a string*/
 }
 
+function validationform() {
+     let nameform = document.getElementById('name');
+     let email = document.getElementById('email');
+     let message = document.getElementById('messageform');
 
+     nameform.value = nameform.value.trim();
+     email.value = email.value.trim();
+     message.value = message.value.trim();
 
-
-/* function check(e){
-     e.preventDefault();
-     ValidateImputs();
-}
-
-function ValidateImputs() {
-     const nameValue = nameform.value.trim();
-     const emailvalue = email.value.trim();
-     const message = form.message.value.trim();
-
-
-     if(nameValue === '') {
+     if(nameform.value === '') {
           SetError(nameform,'Username is required');
      } else {
           SetSuccess (nameform);
      }
-     if (emailvalue=== '') {
-          SetError(email,'Username is required');
-     } else if(!isValidEmail(emailvalue)) {
+     if (email.value=== '') {
+          SetError(email,'email is required');
+     } else if(!isValidEmail(email.value)) {
           SetError(email,'provide a valid email adress');
      } else {
           SetSuccess (email);
      }
 
+     if (message.value=== '') {
+          SetError(message,'message is required');
+     } else {
+          SetSuccess (message);
+     }
+
+     let isValid=true;
+     if(nameform.classList.contains('error') || email.classList.contains('error') || message.classList.contains('error')) {
+          isValid = false
+     }
+     return isValid
 }
 
 
 function isValidEmail (email) {
-     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+     const re =/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+     //const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
      return re.test(String(email).toLowerCase());
  }
 
 function SetError (element, message) {
      const inputControl = element.parentElement;
-     const errorDisplay = inputControl.querySelector('.error');
+     const errorDisplay = element.previousElementSibling;
 
      errorDisplay.innerText = message;
-     inputControl.classList.add('error');
-     inputControl.classList.remove('success');
+     element.classList.add('error');
+     element.classList.remove('success');
 }
 
 function SetSuccess (element) {
      const inputControl = element.parentElement;
-     const errorDisplay = inputControl.querySelector('.error');
+     const errorDisplay = element.previousElementSibling;
 
-     errorDisplay.innertext = '';
-     inputControl.classlist.add('success');
-     inputControl.classlist.remove('error');
-} */
+     errorDisplay.innerText='';
+     element.classList.add('success');
+     element.classList.remove('error');
+}
+
+function checkEmail(){
+     let str = emailElement.value;
+
+     const errorDisplay = emailElement.previousElementSibling;
+     if(str!==str.toLowerCase()){
+          errorDisplay.innerText='Email has to be in lower case';
+          emailElement.classList.add('error');
+          emailElement.classList.remove('success');
+     } else{
+          errorDisplay.innerText='';
+          emailElement.classList.remove('error');
+     }
+}
+
+const emailElement = document.getElementById('email');
+emailElement.addEventListener('input', checkEmail);
